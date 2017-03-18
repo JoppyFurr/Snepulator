bc52f8e18c3e82dec32a1233c7785de81cbe32f3
Joppy Furr <joppyfurr@gmail.com>
2017-03-19 02:28:50 +1300

    sms_vdp, debug: Tidy-up + Name-table debug

--- Source/gpu/sega_vdp.c
+++ Source/gpu/sega_vdp.c
@@ -92,6 +92,27 @@ void vdp_dump (void)
     }
     fprintf (stdout, "\n");
 
+    /* Name Table */
+    fprintf (stdout, "Name Table:\n");
+    uint16_t name_table_base = (((uint16_t) vdp_regs.name_table_addr) << 10) & 0x3800;
+
+
+    for (uint32_t tile_y = 0; tile_y < 28; tile_y++)
+    {
+        fprintf (stdout, "Row %02d:", tile_y);
+        for (uint32_t tile_x = 0; tile_x < 32; tile_x++)
+        {
+            uint16_t tile_address = name_table_base | (tile_y << 6) | (tile_x << 1);
+
+            uint16_t tile = (uint16_t)(vram [tile_address]) +
+                            (((uint16_t)(vram [tile_address + 1])) << 8);
+
+            fprintf (stdout, " %04x", tile);
+
+        }
+        fprintf (stdout, "\n");
+    }
+
 }
 
 static bool first_byte_received = false;
@@ -112,25 +133,20 @@ void vdp_data_write (uint8_t value)
         case VDP_CODE_VRAM_READ:
             fprintf (stdout, "[DEBUG(vdp)]: vdp_data_write: VRAM_READ not implemented.\n");
             break;
+
         case VDP_CODE_VRAM_WRITE:
-            if (value != 0x00 && value != 0xff)
-            {
-                fprintf (stdout, "[DEBUG(vdp)]: vdp_data_write: VRAM_WRITE [%04x] <- %02x.\n",
-                         vdp_regs.address, value);
-            }
+            fprintf (stdout, "[DEBUG(vdp)]: WRITING TO VRAM [%04x] <- %02x.\n", vdp_regs.address, value);
             vram[vdp_regs.address] = value;
             break;
+
         case VDP_CODE_REG_WRITE:
             fprintf (stdout, "[DEBUG(vdp)]: vdp_data_write: REG_WRITE not implemented.\n");
             break;
+
         case VDP_CODE_CRAM_WRITE:
-            if (value != 0x00 && value != 0xff)
-            {
-                fprintf (stdout, "[DEBUG(vdp)]: vdp_data_write: CRAM_WRITE [%02x] <- %02x.\n",
-                         vdp_regs.address & 0x1f, value);
-            }
             cram[vdp_regs.address & 0x1f] = value;
             break;
+
         default:
             break;
     }
@@ -163,18 +179,14 @@ void vdp_control_write (uint8_t value)
                 fprintf (stdout,"[DEBUG(vdp)]: vdp_control_write () VRAM_READ not implemented.\n");
                 break;
             case VDP_CODE_VRAM_WRITE:
-                fprintf (stdout,"[DEBUG(vdp)]: vdp_control_write () VRAM_WRITE.\n");
                 break;
             case VDP_CODE_REG_WRITE:
-                fprintf (stdout, "[DEBUG(vdp)]: VDP REG_WRITE [%01x] <- %02x.\n",
-                         value & 0x0f, vdp_regs.address & 0xff);
                 if ((value & 0x0f) <= 10)
                 {
                     ((uint8_t *) &vdp_regs) [value & 0x0f] = vdp_regs.address & 0x00ff;
                 }
                 break;
             case VDP_CODE_CRAM_WRITE:
-                fprintf (stdout,"[DEBUG(vdp)]: vdp_control_write () CRAM_WRITE.\n");
                 break;
             default:
                 break;
@@ -182,17 +194,20 @@ void vdp_control_write (uint8_t value)
     }
 }
 
-void vdp_render_pattern (Vdp_Pattern *pattern, Vdp_Palette palette, uint32_t x, uint32_t y)
+void vdp_render_pattern (Vdp_Pattern *pattern_base, Vdp_Palette palette, uint32_t x, uint32_t y)
 {
-    for (uint32_t pattern_x = 0; pattern_x < 8; pattern_x++)
+    for (uint32_t pattern_y = 0; pattern_y < 8; pattern_y++)
     {
-        for (uint32_t pattern_y = 0; pattern_y < 8; pattern_y++)
+        char *line_base = (char *)(&pattern_base[pattern_y * 4]);
+        for (uint32_t pattern_x = 0; pattern_x < 8; pattern_x++)
         {
-            uint8_t pixel = (cram + ((palette == VDP_PALETTE_SPRITE) ? 16 : 0))
-                            [ ((pattern->data[pattern_y * 4 + 0 ] & (8 >> pattern_x)) ? (1 << 0) : 0) |
-                              ((pattern->data[pattern_y * 4 + 1 ] & (8 >> pattern_x)) ? (2 << 0) : 0) |
-                              ((pattern->data[pattern_y * 4 + 2 ] & (8 >> pattern_x)) ? (3 << 0) : 0) |
-                              ((pattern->data[pattern_y * 4 + 3 ] & (8 >> pattern_x)) ? (4 << 0) : 0) ];
+            uint8_t bit0 = (line_base[0] & (1 << pattern_x)) ? 0x01 : 0x00;
+            uint8_t bit1 = (line_base[1] & (1 << pattern_x)) ? 0x02 : 0x00;
+            uint8_t bit2 = (line_base[2] & (1 << pattern_x)) ? 0x04 : 0x00;
+            uint8_t bit3 = (line_base[3] & (1 << pattern_x)) ? 0x08 : 0x00;
+
+            /* TODO: Support the sprite palette */
+            uint8_t pixel = cram[((palette == VDP_PALETTE_SPRITE) ? 16 : 0) + (bit0 | bit1 | bit2 | bit3)];
 
             SDL_SetRenderDrawColor (renderer, VDP_TO_RED (pixel),
                                               VDP_TO_GREEN (pixel),
@@ -231,23 +246,28 @@ void vdp_render (void)
 
             /* For other resolutions we use only two bits */
             {
-                uint16_t *name_table_base = (uint16_t *)
-                                                (vram + (((vdp_regs.name_table_addr & (1 << 3)) ? (1 << 13) : 0) |
-                                                         ((vdp_regs.name_table_addr & (1 << 2)) ? (1 << 12) : 0) |
-                                                         ((vdp_regs.name_table_addr & (1 << 1)) ? (1 << 11) : 0)));
-                for (uint32_t tile_x = 0; tile_x < 32; tile_x++)
-                {
-                    for (uint32_t tile_y = 0; tile_y < 28; tile_y++)
-                    {
-                        if (tile_y >= 24) /* TODO: Do this properly once scrolling is implemented */
-                            continue;
+                uint16_t name_table_base = (((uint16_t) vdp_regs.name_table_addr) << 10) & 0x3800;
 
-                        uint16_t tile = name_table_base[tile_x + 32 * tile_y];
-                        Vdp_Pattern pattern = ((Vdp_Pattern *)vram)[tile * 0x01ff];
+                for (uint32_t tile_y = 0; tile_y < 28; tile_y++)
+                {
+                    if (tile_y >= 24) /* TODO: Do this properly once scrolling is implemented */
+                        continue;
+
+                    for (uint32_t tile_x = 0; tile_x < 32; tile_x++)
+                    {
+
+                        uint16_t tile_address = name_table_base | (tile_y << 6) | (tile_x << 1);
+
+                        uint16_t tile = (uint16_t)(vram [tile_address]) +
+                                        (((uint16_t)(vram [tile_address + 1])) << 8);
+
+                        Vdp_Pattern *pattern = (Vdp_Pattern *) &vram[(tile & 0x1ff) * sizeof(Vdp_Pattern)];
+
                         Vdp_Palette palette = (tile & (1 << 11)) ? VDP_PALETTE_SPRITE : VDP_PALETTE_BACKGROUND;
+
                         /* TODO: Flip flags */
                         /* TODO: Priority flag */
-                        vdp_render_pattern (&pattern, palette, VDP_OVERSCAN_X + 8 * tile_x,
+                        vdp_render_pattern (pattern, palette, VDP_OVERSCAN_X + 8 * tile_x,
                                                                VDP_OVERSCAN_Y + 8 * tile_y);
 
                     }
