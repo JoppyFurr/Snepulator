9530fc733b99c69449cabd3039571fcc17877208
Joppy Furr <joppyfurr@gmail.com>
2017-03-27 23:10:12 +1300

    Z80: Move bit operations to their own function

--- Source/cpu/z80.h
+++ Source/cpu/z80.h
@@ -68,9 +68,9 @@ typedef struct Z80_Regs_t {
 #define Z80_FLAG_SIGN            (1 << 7)
 
 /* Function declarations */
-void z80_reset ();
+uint32_t z80_init (uint8_t (* _memory_read) (uint16_t),
+                   void    (* _memory_write)(uint16_t, uint8_t),
+                   uint8_t (* _io_read)     (uint8_t),
+                   void    (* _io_write)    (uint8_t, uint8_t));
 
-uint32_t z80_run (uint8_t (* memory_read) (uint16_t),
-              void    (* memory_write)(uint16_t, uint8_t),
-              uint8_t (* io_read)     (uint8_t),
-              void    (* io_write)    (uint8_t, uint8_t));
+uint32_t z80_run ();
