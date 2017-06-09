602472cd758985e5a1fcdeb4a516a407637a82d0
Joppy Furr <joppyfurr@gmail.com>
2017-06-10 00:40:08 +1200

    sms_vdp: General code tidy up
      -> Improve variable names in vdp_render_pattern
      -> Improve register descriptions in sega_vdp.h

--- Source/gpu/sega_vdp.h
+++ Source/gpu/sega_vdp.h
@@ -12,23 +12,49 @@ typedef enum Vdp_Palette_t {
     VDP_PALETTE_SPRITE,
 } Vdp_Palette;
 
+/* TODO: Consolidate helpers like this into a single file */
+#define BIT_0               (1 << 0)
+#define BIT_1               (1 << 1)
+#define BIT_2               (1 << 2)
+#define BIT_3               (1 << 3)
+#define BIT_4               (1 << 4)
+#define BIT_5               (1 << 5)
+#define BIT_6               (1 << 6)
+#define BIT_7               (1 << 7)
+
 /* Structs */
 typedef struct Vdp_Regs_t {
     uint8_t  mode_ctrl_1;
+#define VDP_MODE_CTRL_1_NO_SYNC         BIT_0
+#define VDP_MODE_CTRL_1_MODE_2          BIT_1
+#define VDP_MODE_CTRL_1_MODE_4          BIT_2
+#define VDP_MODE_CTRL_1_EC              BIT_3
+#define VDP_MODE_CTRL_1_LINE_INT_EN     BIT_4
+#define VDP_MODE_CTRL_1_MASK_COL_1      BIT_5
+#define VDP_MODE_CTRL_1_VLOCK_0_1       BIT_6
+#define VDP_MODE_CTRL_1_HLOCK_24_31     BIT_7
     uint8_t  mode_ctrl_2;
-    uint8_t  name_table_addr;
-    uint8_t  colour_table_addr;          /* Unused - Bits 0xff should be set. */
-    uint8_t  background_pattern_addr;    /* Unused - Bits 0x07 should be set. */
-    uint8_t  sprite_attr_table_addr;
-    uint8_t  sprite_pattern_table_addr;
-    uint8_t  background_colour;          /* Bits 0x0f select the background from the sprite palette. */
-    uint8_t  background_x_scroll;
-    uint8_t  background_y_scroll;
-    uint8_t  line_counter;
+#define VDP_MODE_CTRL_2_SPRITE_DOUBLE   BIT_0
+#define VDP_MODE_CTRL_2_SPRITE_WIDE     BIT_1
+#define VDP_MODE_CTRL_2_MODE_3          BIT_3
+#define VDP_MODE_CTRL_2_MODE_1          BIT_4
+#define VDP_MODE_CTRL_2_FRAME_INT_EN    BIT_5
+#define VDP_MODE_CTRL_2_BLK             BIT_6
+    uint8_t  name_table_addr;           /* Bits 0x0e select name table base address. */
+    uint8_t  colour_table_addr;         /* Unused - Bits 0xff should be set. */
+    uint8_t  background_pattern_addr;   /* Unused - Bits 0x07 should be set. */
+    uint8_t  sprite_attr_table_addr;    /* Bits 0x7e select the sprite attribute table base address. */
+    uint8_t  sprite_pattern_table_addr; /* Bits 0x01 selects sprite pattern generator base address. */
+    uint8_t  background_colour;         /* Bits 0x0f select the background from the sprite palette. */
+    uint8_t  background_x_scroll;       /* Horizontal scroll */
+    uint8_t  background_y_scroll;       /* Vertical scroll */
+    uint8_t  line_counter;              /* Line interrupt counter */
+
     uint8_t  code;
     uint16_t address;
     uint8_t  read_buffer;
     uint8_t  status;
+#define VDP_STATUS_INT                  BIT_7
     uint8_t  line_interrupt_counter;
     bool     line_interrupt;
 } Vdp_Regs;
@@ -41,9 +67,6 @@ typedef struct Vdp_Pattern_t {
 #define VDP_OVERSCAN_X 8
 #define VDP_OVERSCAN_Y 16
 
-/* Register bits */
-#define VDP_MODE_CTRL_1_MODE_4  (1 << 2)
-#define VDP_STATUS_INT (1 << 7)
 
 /* Functions */
 uint8_t vdp_data_read ();
