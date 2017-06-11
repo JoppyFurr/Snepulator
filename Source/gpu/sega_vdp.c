a8569ab59370a1cf6490db0dddcab10d9b4efee0
Joppy Furr <joppyfurr@gmail.com>
2017-06-12 00:43:56 +1200

    VDP: Initial sprite implementation

--- Source/gpu/sega_vdp.c
+++ Source/gpu/sega_vdp.c
@@ -211,39 +211,6 @@ void vdp_control_write (uint8_t value)
     }
 }
 
-extern float sms_vdp_texture_data [256 * 256 * 3];
-extern float sms_vdp_background [3];
-#define VDP_STRIDE_X (3)
-#define VDP_STRIDE_Y (256 * 3)
-
-typedef struct Point2D_s {
-    int32_t x;
-    int32_t y;
-} Point2D;
-
-void vdp_render_pattern (Vdp_Pattern *pattern_base, Vdp_Palette palette, Point2D offset)
-{
-    for (uint32_t y = 0; y < 8; y++)
-    {
-        char *line_base = (char *)(&pattern_base->data[y * 4]);
-        for (uint32_t x = 0; x < 8; x++)
-        {
-            if (x + offset.x > 255)
-                continue;
-            uint8_t bit0 = (line_base[0] & (0x80 >> x)) ? 0x01 : 0x00;
-            uint8_t bit1 = (line_base[1] & (0x80 >> x)) ? 0x02 : 0x00;
-            uint8_t bit2 = (line_base[2] & (0x80 >> x)) ? 0x04 : 0x00;
-            uint8_t bit3 = (line_base[3] & (0x80 >> x)) ? 0x08 : 0x00;
-
-            uint8_t pixel = cram[((palette == VDP_PALETTE_SPRITE) ? 16 : 0) + (bit0 | bit1 | bit2 | bit3)];
-
-            sms_vdp_texture_data [(offset.x + x) * VDP_STRIDE_X + (offset.y + y) * VDP_STRIDE_Y + 0] = VDP_TO_RED   (pixel) / 256.0f;
-            sms_vdp_texture_data [(offset.x + x) * VDP_STRIDE_X + (offset.y + y) * VDP_STRIDE_Y + 1] = VDP_TO_GREEN (pixel) / 256.0f;
-            sms_vdp_texture_data [(offset.x + x) * VDP_STRIDE_X + (offset.y + y) * VDP_STRIDE_Y + 2] = VDP_TO_BLUE  (pixel) / 256.0f;
-        }
-    }
-}
-
 /* TODO: For now, assuming 256x192 PAL */
 /* 50 frames per second, 313 scanlines per frame */
 
@@ -320,6 +287,133 @@ bool vdp_get_interrupt (void)
     return false;
 }
 
+extern float sms_vdp_texture_data [256 * 256 * 3];
+extern float sms_vdp_background [3];
+#define VDP_STRIDE_X (3)
+#define VDP_STRIDE_Y (256 * 3)
+
+typedef struct Point2D_s {
+    int32_t x;
+    int32_t y;
+} Point2D;
+
+void vdp_render_pattern (Vdp_Pattern *pattern_base, Vdp_Palette palette, Point2D offset, bool transparency)
+{
+    for (uint32_t y = 0; y < 8; y++)
+    {
+        char *line_base = (char *)(&pattern_base->data[y * 4]);
+        for (uint32_t x = 0; x < 8; x++)
+        {
+            if (x + offset.x > 255)
+                continue;
+            uint8_t bit0 = (line_base[0] & (0x80 >> x)) ? 0x01 : 0x00;
+            uint8_t bit1 = (line_base[1] & (0x80 >> x)) ? 0x02 : 0x00;
+            uint8_t bit2 = (line_base[2] & (0x80 >> x)) ? 0x04 : 0x00;
+            uint8_t bit3 = (line_base[3] & (0x80 >> x)) ? 0x08 : 0x00;
+
+            if (transparency == true && (bit0 | bit1 | bit2 | bit3) == 0)
+                continue;
+
+            uint8_t pixel = cram[((palette == VDP_PALETTE_SPRITE) ? 16 : 0) + (bit0 | bit1 | bit2 | bit3)];
+
+            sms_vdp_texture_data [(offset.x + x) * VDP_STRIDE_X + (offset.y + y) * VDP_STRIDE_Y + 0] = VDP_TO_RED   (pixel) / 256.0f;
+            sms_vdp_texture_data [(offset.x + x) * VDP_STRIDE_X + (offset.y + y) * VDP_STRIDE_Y + 1] = VDP_TO_GREEN (pixel) / 256.0f;
+            sms_vdp_texture_data [(offset.x + x) * VDP_STRIDE_X + (offset.y + y) * VDP_STRIDE_Y + 2] = VDP_TO_BLUE  (pixel) / 256.0f;
+        }
+    }
+}
+
+/* TODO: Find out how transparent pixels work */
+void vdp_render_background (bool priority)
+{
+    /* TODO: If using more than 192 lines, only two bits should be used here */
+    uint16_t name_table_base = (((uint16_t) vdp_regs.name_table_addr) << 10) & 0x3800;
+
+    uint8_t start_column = 32 - ((vdp_regs.background_x_scroll & 0xf8) >> 3);
+    uint8_t fine_scroll_x = vdp_regs.background_x_scroll & 0x07;
+    uint8_t start_row = 32 - ((vdp_regs.background_y_scroll & 0xf8) >> 3);
+    uint8_t fine_scroll_y = vdp_regs.background_y_scroll & 0x07;
+    Point2D position;
+
+    /* TODO: Populate the left of the screen with the background colour for fine-scroll coverage */
+    /* TODO: Implement VDP_MODE_CTRL_1_HLOCK_24_31 */
+    /* TODO: Implement VDP_MODE_CTRL_1_VLOCK_0_1   */
+    /* TODO: Implement VDP_MODE_CTRL_2_BLK         */
+    /* TODO: For 192 lines, the Y scroll value should wrap at 224 */
+    /* TODO: The vertical scroll value should only take affect between active frames, not mid-frame */
+    /* TODO: If fine-scroll of Y is active, what happens in the first few lines of the display? */
+    for (uint32_t tile_y = 0; tile_y < 28; tile_y++)
+    {
+        if (tile_y >= 24) /* TODO: Do this properly once scrolling is implemented */
+            continue;
+
+        for (uint32_t tile_x = 0; tile_x < 32; tile_x++)
+        {
+            uint16_t tile_address = name_table_base | ((tile_y + start_row) << 6) | ((tile_x + start_column) % 32 << 1);
+
+            uint16_t tile = ((uint16_t)(vram [tile_address])) +
+                            (((uint16_t)(vram [tile_address + 1])) << 8);
+
+            /* If we are rendering the "priority" layer, skip any non-priority tiles */
+            if (priority && !(tile & 0x1000))
+                continue;
+
+            Vdp_Pattern *pattern = (Vdp_Pattern *) &vram[(tile & 0x1ff) * sizeof(Vdp_Pattern)];
+
+            Vdp_Palette palette = (tile & (1 << 11)) ? VDP_PALETTE_SPRITE : VDP_PALETTE_BACKGROUND;
+
+            /* TODO: Flip flags */
+            position.x = 8 * tile_x + fine_scroll_x;
+            position.y = 8 * tile_y + fine_scroll_y;
+            vdp_render_pattern (pattern, palette, position, false);
+
+        }
+    }
+}
+
+/* TODO: Limit of eight sprites per line / set sprite-overflow flag */
+/* TODO: Collision detection */
+/* TODO: VDP Flag BIT_3 of ctrl_1 subtracts 8 from x position of sprites */
+/* TODO: Pixel-doubling */
+/* TODO: Draw order: the first entry in the line's sprite buffer with a non-transparent pixel is the one that sticks */
+void vdp_render_sprites (void)
+{
+
+    uint16_t sprite_attribute_table_base = (((uint16_t) vdp_regs.sprite_attr_table_addr) << 7) & 0x3f00;
+    uint16_t sprite_pattern_generator_base = (((uint16_t) vdp_regs.sprite_pattern_generator_addr) << 13) & 0x2000;
+    Vdp_Pattern *pattern;
+    Point2D position;
+
+    for (int i = 0; i < 64; i++)
+    {
+        uint8_t y = vram [sprite_attribute_table_base + i];
+        uint8_t x = vram [sprite_attribute_table_base + 0x80 + i * 2];
+        uint8_t pattern_index = vram [sprite_attribute_table_base + 0x80 + i * 2 + 1];
+
+        /* Break if there are no more sprites */
+        if (y == 0xd0)
+            break;
+
+        position.x = x;
+        position.y = y + 1;
+
+        if (vdp_regs.mode_ctrl_2 & VDP_MODE_CTRL_2_SPRITE_TALL)
+            pattern_index &= 0xfe;
+
+        pattern = (Vdp_Pattern *) &vram [sprite_pattern_generator_base + pattern_index * sizeof (Vdp_Pattern)];
+        vdp_render_pattern (pattern, VDP_PALETTE_SPRITE, position, true);
+
+        if (vdp_regs.mode_ctrl_2 & VDP_MODE_CTRL_2_SPRITE_TALL)
+        {
+            position.y += 8;
+            pattern = (Vdp_Pattern *) &vram [sprite_pattern_generator_base + (pattern_index + 1) * sizeof (Vdp_Pattern)];
+            vdp_render_pattern (pattern, VDP_PALETTE_SPRITE, position, true);
+        }
+    }
+}
+
+/* Note: For now, we will assume 192 lines, as this is what the vast majority of
+ *       SMS games actually use */
 void vdp_render (void)
 {
     /* Background / overscan - Is this specific to mode 4? */
@@ -333,61 +427,16 @@ void vdp_render (void)
     {
         /* Mode 4 */
         case VDP_MODE_CTRL_1_MODE_4:
-            /* Draw order:
-             *  Non-priority background
-             *    Patterns are used for both sprites and the background. Each pattern is 8x8 pixels.
-             *    There is enough VRAM for 512 patterns, but some is used for tables. Each pattern
-             *    uses 32 bytes. Four bytes per row. Each byte being one bit for each pixel.
-             *    While not documented, I assume the patterns just start at zero?
-             *    The name table  is a 2D matrix of 16-bit words selecting a pattern and palette from VRAM.
-             *  sprites
-             *  priority background */
-
-            /* Background:
-             * For 192 lines, the background is made up of 32×28 tiles.
-             * For more than 192 lines, the background is made up of 32×32 tiles. */
-            {
-                /* TODO: If using more than 192 lines, only two bits should be used here */
-                uint16_t name_table_base = (((uint16_t) vdp_regs.name_table_addr) << 10) & 0x3800;
-
-                uint8_t start_column = 32 - ((vdp_regs.background_x_scroll & 0xf8) >> 3);
-                uint8_t fine_scroll_x = vdp_regs.background_x_scroll & 0x07;
-                uint8_t start_row = 32 - ((vdp_regs.background_y_scroll & 0xf8) >> 3);
-                uint8_t fine_scroll_y = vdp_regs.background_y_scroll & 0x07;
-                Point2D position;
-
-                /* TODO: Populate the left of the screen with the background colour for fine-scroll coverage */
-                /* TODO: Implement VDP_MODE_CTRL_1_HLOCK_24_31 */
-                /* TODO: Implement VDP_MODE_CTRL_1_VLOCK_0_1   */
-                /* TODO: Implement VDP_MODE_CTRL_2_BLK         */
-                /* TODO: For 192 lines, the Y scroll value should wrap at 224 */
-                /* TODO: The vertical scroll value should only take affect between active frames, not mid-frame */
-                /* TODO: If fine-scroll of Y is active, what happens in the first few lines of the display? */
-                for (uint32_t tile_y = 0; tile_y < 28; tile_y++)
+            for (int y = 0; y < 192; y++)
+                for (int x = 0; x < 256; x++)
                 {
-                    if (tile_y >= 24) /* TODO: Do this properly once scrolling is implemented */
-                        continue;
-
-                    for (uint32_t tile_x = 0; tile_x < 32; tile_x++)
-                    {
-                        uint16_t tile_address = name_table_base | ((tile_y + start_row) << 6) | ((tile_x + start_column) % 32 << 1);
-
-                        uint16_t tile = ((uint16_t)(vram [tile_address])) +
-                                        (((uint16_t)(vram [tile_address + 1])) << 8);
-
-                        Vdp_Pattern *pattern = (Vdp_Pattern *) &vram[(tile & 0x1ff) * sizeof(Vdp_Pattern)];
-
-                        Vdp_Palette palette = (tile & (1 << 11)) ? VDP_PALETTE_SPRITE : VDP_PALETTE_BACKGROUND;
-
-                        /* TODO: Flip flags */
-                        /* TODO: Priority flag */
-                        position.x = 8 * tile_x + fine_scroll_x;
-                        position.y = 8 * tile_y + fine_scroll_y;
-                        vdp_render_pattern (pattern, palette, position);
-
-                    }
+                        sms_vdp_texture_data [x * VDP_STRIDE_X + y * VDP_STRIDE_Y + 0] = sms_vdp_background[0];
+                        sms_vdp_texture_data [x * VDP_STRIDE_X + y * VDP_STRIDE_Y + 1] = sms_vdp_background[1];
+                        sms_vdp_texture_data [x * VDP_STRIDE_X + y * VDP_STRIDE_Y + 2] = sms_vdp_background[2];
                 }
-            }
+            vdp_render_background (false);
+            vdp_render_sprites ();
+            vdp_render_background (true);
             break;
 
         /* TMS9918 mode */
@@ -396,6 +445,7 @@ void vdp_render (void)
             break;
     }
 
+    /* TODO: Investigate using a NPOT texture to remove this */
     /* Populate a stripe of overscan below the VDP texture, as OpenGL likes a power-of-two size
      * and will try use this line rather than GL_TEXTURE_BORDER_COLOR. */
     for (int i = 0; i < 256; i++)
