43890527e53d84868a63c87ac1996de0a9d11796
Joppy Furr <joppyfurr@gmail.com>
2017-06-18 23:25:15 +1200

    sn76489: Initial implementation of the SN76489 (square waves only)

---
+++ Source/audio/sn79489.h
@@ -0,0 +1,34 @@
+
+typedef struct SN79489_Regs_t {
+
+    /* 4-bit volume registers */
+    uint8_t vol_0;
+    uint8_t vol_1;
+    uint8_t vol_2;
+    uint8_t vol_3;
+
+    /* 10-bit tone registers */
+    uint16_t tone_0;
+    uint16_t tone_1;
+    uint16_t tone_2;
+
+    /* 4-bit noise register */
+    uint8_t noise;
+
+    /* Counters */
+    int16_t counter_0;
+    int16_t counter_1;
+    int16_t counter_2;
+    int16_t counter_3;
+
+    /* Outputs */
+    int16_t output_0;
+    int16_t output_1;
+    int16_t output_2;
+    int16_t output_3;
+
+} SN79489_Regs;
+
+void sn79489_get_samples (int16_t *stream, int len);
+void sn79489_data_write (uint8_t data);
+void sn79489_init (void);
