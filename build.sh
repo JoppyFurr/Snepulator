0794342a8d21f596bd33c053bd740d10aac12f00
Joppy Furr <joppyfurr@gmail.com>
2018-06-29 00:16:57 +1200

    maint: Clang support for build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+# Select compiler
+COMPILER=${1:-"gcc"}
+
+if [ $COMPILER = clang ] ; then
+    echo "Using Clang"
+    CC=clang
+    CXX=clang++
+else
+    echo "Using GCC"
+    CC=gcc
+    CXX=g++
+fi
+
 # Create a build directory if it does not already exist.
 mkdir -p Work
 
@@ -7,13 +20,15 @@ mkdir -p Work
 rm Work/*.o
 
 # Compile C11 code.
-gcc -c Source/cpu/z80.c         -o Work/z80.o       -std=c11 -Wall -Werror
-gcc -c Source/gpu/sega_vdp.c    -o Work/sega_vdp.o  -std=c11 -Wall -Werror
-gcc -c Source/sms.c             -o Work/sms.o       -std=c11 -Wall -Werror
-gcc -c Source/audio/sn79489.c   -o Work/sn79489.o   -std=c11 -Wall -Werror
+eval $CC -c Source/cpu/z80.c         -o Work/z80.o       -std=c11 -Wall -Werror
+eval $CC -c Source/gpu/sega_vdp.c    -o Work/sega_vdp.o  -std=c11 -Wall -Werror
+eval $CC -c Source/sms.c             -o Work/sms.o       -std=c11 -Wall -Werror
+eval $CC -c Source/audio/sn79489.c   -o Work/sn79489.o   -std=c11 -Wall -Werror
 
 # Compile C++11 GUI and link to the rest of the code.
-g++ Source/*.cpp \
+DATE=`date --rfc-3339=date`
+eval $CXX \
+    Source/*.cpp \
     Work/*.o \
     Libraries/imgui-1.49/*.cpp \
     Libraries/imgui-1.49/examples/libs/gl3w/GL/gl3w.c \
@@ -22,5 +37,5 @@ g++ Source/*.cpp \
     -I Libraries/imgui-1.49/ \
     -I Libraries/imgui-1.49/examples/libs/gl3w/ \
     `sdl2-config --libs` \
-    -lGL -ldl -DBUILD_DATE=\"`date --rfc-3339=date`\" \
+    -lGL -ldl -DBUILD_DATE=\\\"$DATE\\\" \
     -o Snepulator -std=c++11
