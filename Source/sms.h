9aa238f067a46c6e2b9661d16170c3c0d5648507
Joppy Furr <joppyfurr@gmail.com>
2020-10-11 00:20:35 +1300

    SMS: Initial anaglpyh 3D support

--- Source/sms.h
+++ Source/sms.h
@@ -12,5 +12,11 @@ typedef enum SMS_Mapper_e {
     SMS_MAPPER_KOREAN,
 } SMS_Mapper;
 
+typedef enum SMS_3D_Field_e {
+    SMS_3D_FIELD_NONE = 0,
+    SMS_3D_FIELD_LEFT,
+    SMS_3D_FIELD_RIGHT
+} SMS_3D_Field;
+
 /* Reset the SMS and load a new BIOS and/or cartridge ROM. */
 void sms_init ();
