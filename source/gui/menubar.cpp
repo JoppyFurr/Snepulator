42a39d1652129c93689550b9b000185e6dd84d98
Joppy Furr <joppyfurr@gmail.com>
2021-04-24 11:27:36 +1200

    GUI: Re-order menubar

--- source/gui/menubar.cpp
+++ source/gui/menubar.cpp
@@ -137,116 +137,6 @@ void snepulator_render_menubar (void)
             ImGui::EndMenu ();
         }
 
-        if (ImGui::BeginMenu ("Audio"))
-        {
-            if (ImGui::BeginMenu ("Device"))
-            {
-                int count = SDL_GetNumAudioDevices (0);
-                for (int i = 0; i < count; i++)
-                {
-                    const char *audio_device_name = SDL_GetAudioDeviceName (i, 0);
-                    if (audio_device_name == NULL)
-                        audio_device_name = "Unknown Audio Device";
-
-                    if (ImGui::MenuItem (audio_device_name, NULL)) { }
-                }
-                ImGui::EndMenu ();
-            }
-            ImGui::EndMenu ();
-        }
-
-        if (ImGui::BeginMenu ("Video"))
-        {
-            if (ImGui::BeginMenu ("Filter"))
-            {
-                if (ImGui::MenuItem ("Dot Matrix",  NULL, state.video_filter == VIDEO_FILTER_DOT_MATRIX))
-                {
-                    state.video_filter = VIDEO_FILTER_DOT_MATRIX;
-                    config_string_set ("video", "filter", "Dot Matrix");
-                    config_write ();
-                }
-                if (ImGui::MenuItem ("Linear Interpolation",  NULL, state.video_filter == VIDEO_FILTER_LINEAR))
-                {
-                    state.video_filter = VIDEO_FILTER_LINEAR;
-                    config_string_set ("video", "filter", "Linear");
-                    config_write ();
-                }
-                if (ImGui::MenuItem ("Nearest Neighbour", NULL, state.video_filter == VIDEO_FILTER_NEAREST))
-                {
-                    state.video_filter = VIDEO_FILTER_NEAREST;
-                    config_string_set ("video", "filter", "Nearest");
-                    config_write ();
-                }
-                if (ImGui::MenuItem ("Scanlines",  NULL, state.video_filter == VIDEO_FILTER_SCANLINES))
-                {
-                    state.video_filter = VIDEO_FILTER_SCANLINES;
-                    config_string_set ("video", "filter", "Scanlines");
-                    config_write ();
-                }
-                ImGui::EndMenu ();
-            }
-
-            if (ImGui::BeginMenu ("3D Mode"))
-            {
-                if (ImGui::MenuItem ("Left image only", NULL, state.video_3d_mode == VIDEO_3D_LEFT_ONLY))
-                {
-                    state.video_3d_mode = VIDEO_3D_LEFT_ONLY;
-                }
-                if (ImGui::MenuItem ("Right image only", NULL, state.video_3d_mode == VIDEO_3D_RIGHT_ONLY))
-                {
-                    state.video_3d_mode = VIDEO_3D_RIGHT_ONLY;
-                }
-                if (ImGui::MenuItem ("Red-Cyan", NULL, state.video_3d_mode == VIDEO_3D_RED_CYAN))
-                {
-                    state.video_3d_mode = VIDEO_3D_RED_CYAN;
-                }
-                if (ImGui::MenuItem ("Red-Green", NULL, state.video_3d_mode == VIDEO_3D_RED_GREEN))
-                {
-                    state.video_3d_mode = VIDEO_3D_RED_GREEN;
-                }
-                if (ImGui::MenuItem ("Magenta-Green", NULL, state.video_3d_mode == VIDEO_3D_MAGENTA_GREEN))
-                {
-                    state.video_3d_mode = VIDEO_3D_MAGENTA_GREEN;
-                }
-
-                ImGui::Separator ();
-
-                if (ImGui::BeginMenu ("Colour"))
-                {
-                    if (ImGui::MenuItem ("Saturation 0%", NULL, state.video_3d_saturation == 0.0))
-                    {
-                        state.video_3d_saturation = 0.0;
-                    }
-                    if (ImGui::MenuItem ("Saturation 25%", NULL, state.video_3d_saturation == 0.25))
-                    {
-                        state.video_3d_saturation = 0.25;
-                    }
-                    if (ImGui::MenuItem ("Saturation 50%", NULL, state.video_3d_saturation == 0.50))
-                    {
-                        state.video_3d_saturation = 0.50;
-                    }
-                    if (ImGui::MenuItem ("Saturation 75%", NULL, state.video_3d_saturation == 0.75))
-                    {
-                        state.video_3d_saturation = 0.75;
-                    }
-                    if (ImGui::MenuItem ("Saturation 100%", NULL, state.video_3d_saturation == 1.0))
-                    {
-                        state.video_3d_saturation = 1.0;
-                    }
-                    ImGui::EndMenu ();
-                }
-
-                ImGui::EndMenu ();
-            }
-
-            if (ImGui::MenuItem ("Take Screenshot"))
-            {
-                snepulator_take_screenshot ();
-            }
-
-            ImGui::EndMenu ();
-        }
-
         if (ImGui::BeginMenu ("Console"))
         {
             if (ImGui::MenuItem ("Hard Reset"))
@@ -407,6 +297,116 @@ void snepulator_render_menubar (void)
                 input_start ();
                 input_modal = true;
             }
+            ImGui::EndMenu ();
+        }
+
+        if (ImGui::BeginMenu ("Audio"))
+        {
+            if (ImGui::BeginMenu ("Device"))
+            {
+                int count = SDL_GetNumAudioDevices (0);
+                for (int i = 0; i < count; i++)
+                {
+                    const char *audio_device_name = SDL_GetAudioDeviceName (i, 0);
+                    if (audio_device_name == NULL)
+                        audio_device_name = "Unknown Audio Device";
+
+                    if (ImGui::MenuItem (audio_device_name, NULL)) { }
+                }
+                ImGui::EndMenu ();
+            }
+            ImGui::EndMenu ();
+        }
+
+        if (ImGui::BeginMenu ("Video"))
+        {
+            if (ImGui::BeginMenu ("Filter"))
+            {
+                if (ImGui::MenuItem ("Dot Matrix",  NULL, state.video_filter == VIDEO_FILTER_DOT_MATRIX))
+                {
+                    state.video_filter = VIDEO_FILTER_DOT_MATRIX;
+                    config_string_set ("video", "filter", "Dot Matrix");
+                    config_write ();
+                }
+                if (ImGui::MenuItem ("Linear Interpolation",  NULL, state.video_filter == VIDEO_FILTER_LINEAR))
+                {
+                    state.video_filter = VIDEO_FILTER_LINEAR;
+                    config_string_set ("video", "filter", "Linear");
+                    config_write ();
+                }
+                if (ImGui::MenuItem ("Nearest Neighbour", NULL, state.video_filter == VIDEO_FILTER_NEAREST))
+                {
+                    state.video_filter = VIDEO_FILTER_NEAREST;
+                    config_string_set ("video", "filter", "Nearest");
+                    config_write ();
+                }
+                if (ImGui::MenuItem ("Scanlines",  NULL, state.video_filter == VIDEO_FILTER_SCANLINES))
+                {
+                    state.video_filter = VIDEO_FILTER_SCANLINES;
+                    config_string_set ("video", "filter", "Scanlines");
+                    config_write ();
+                }
+                ImGui::EndMenu ();
+            }
+
+            if (ImGui::BeginMenu ("3D Mode"))
+            {
+                if (ImGui::MenuItem ("Left image only", NULL, state.video_3d_mode == VIDEO_3D_LEFT_ONLY))
+                {
+                    state.video_3d_mode = VIDEO_3D_LEFT_ONLY;
+                }
+                if (ImGui::MenuItem ("Right image only", NULL, state.video_3d_mode == VIDEO_3D_RIGHT_ONLY))
+                {
+                    state.video_3d_mode = VIDEO_3D_RIGHT_ONLY;
+                }
+                if (ImGui::MenuItem ("Red-Cyan", NULL, state.video_3d_mode == VIDEO_3D_RED_CYAN))
+                {
+                    state.video_3d_mode = VIDEO_3D_RED_CYAN;
+                }
+                if (ImGui::MenuItem ("Red-Green", NULL, state.video_3d_mode == VIDEO_3D_RED_GREEN))
+                {
+                    state.video_3d_mode = VIDEO_3D_RED_GREEN;
+                }
+                if (ImGui::MenuItem ("Magenta-Green", NULL, state.video_3d_mode == VIDEO_3D_MAGENTA_GREEN))
+                {
+                    state.video_3d_mode = VIDEO_3D_MAGENTA_GREEN;
+                }
+
+                ImGui::Separator ();
+
+                if (ImGui::BeginMenu ("Colour"))
+                {
+                    if (ImGui::MenuItem ("Saturation 0%", NULL, state.video_3d_saturation == 0.0))
+                    {
+                        state.video_3d_saturation = 0.0;
+                    }
+                    if (ImGui::MenuItem ("Saturation 25%", NULL, state.video_3d_saturation == 0.25))
+                    {
+                        state.video_3d_saturation = 0.25;
+                    }
+                    if (ImGui::MenuItem ("Saturation 50%", NULL, state.video_3d_saturation == 0.50))
+                    {
+                        state.video_3d_saturation = 0.50;
+                    }
+                    if (ImGui::MenuItem ("Saturation 75%", NULL, state.video_3d_saturation == 0.75))
+                    {
+                        state.video_3d_saturation = 0.75;
+                    }
+                    if (ImGui::MenuItem ("Saturation 100%", NULL, state.video_3d_saturation == 1.0))
+                    {
+                        state.video_3d_saturation = 1.0;
+                    }
+                    ImGui::EndMenu ();
+                }
+
+                ImGui::EndMenu ();
+            }
+
+            if (ImGui::MenuItem ("Take Screenshot"))
+            {
+                snepulator_take_screenshot ();
+            }
+
             ImGui::EndMenu ();
         }
 
