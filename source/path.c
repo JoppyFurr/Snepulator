8d78973aa0737e35bf6bb46e613ffc488c12b905
Joppy Furr <joppyfurr@gmail.com>
2022-09-20 00:49:41 +1200

    path: Fix bug where save states ended up in the sram directory

--- source/path.c
+++ source/path.c
@@ -16,7 +16,7 @@
 
 /*
  * Get the directory that the snepulator files reside in.
- * TODO: Check that callers free...
+ * The returned string should not be freed.
  */
 int32_t path_base (char **path_ptr)
 {
@@ -67,44 +67,41 @@ int32_t path_base (char **path_ptr)
 
 /*
  * Get the path to the requested subdirectory.
+ * Intended for internal use, should only be called once per name.
  */
-int32_t path_get_dir (char **path_ptr, char *name)
+static int32_t path_get_dir (char **path_ptr, char *name)
 {
-    static char *path = NULL;
+    char *path;
+    struct stat stat_buf;
+    char *base;
+    int len;
 
-    if (path == NULL)
+    if (path_base (&base) == -1)
     {
-        struct stat  stat_buf;
-        char *base;
-        int len;
+        return -1;
+    }
 
-        if (path_base (&base) == -1)
+    /* Get the path length */
+    len = snprintf (NULL, 0, "%s/%s", base, name) + 1;
+
+    /* Create the string */
+    path = calloc (len, 1);
+    snprintf (path, len, "%s/%s", base, name);
+
+    /* Create the directory if it doesn't exist */
+    if (stat (path, &stat_buf) == -1)
+    {
+        if (errno == ENOENT)
         {
-            return -1;
+            if (mkdir (path, S_IRWXU) == -1)
+            {
+                snepulator_error ("Filesystem Error", "Unable to create directory.");
+            }
         }
-
-        /* Get the path length */
-        len = snprintf (NULL, 0, "%s/%s", base, name) + 1;
-
-        /* Create the string */
-        path = calloc (len, 1);
-        snprintf (path, len, "%s/%s", base, name);
-
-        /* Create the directory if it doesn't exist */
-        if (stat (path, &stat_buf) == -1)
+        else
         {
-            if (errno == ENOENT)
-            {
-                if (mkdir (path, S_IRWXU) == -1)
-                {
-                    snepulator_error ("Filesystem Error", "Unable to create directory.");
-                }
-            }
-            else
-            {
-                snepulator_error ("Filesystem Error", "Unable to stat directory.");
-                return -1;
-            }
+            snepulator_error ("Filesystem Error", "Unable to stat directory.");
+            return -1;
         }
     }
 
@@ -116,18 +113,20 @@ int32_t path_get_dir (char **path_ptr, char *name)
 
 /*
  * Generate the SRAM backup path.
- *
  * This string needs to be freed.
  */
 char *path_sram (uint8_t rom_hash [HASH_LENGTH])
 {
-    static char *path;
-    char *dir;
+    static char *dir = NULL;
+    char *path;
     int len;
 
-    if (path_get_dir (&dir, "sram") == -1)
+    if (dir == NULL)
     {
-        return NULL;
+        if (path_get_dir (&dir, "sram") == -1)
+        {
+            return NULL;
+        }
     }
 
     /* Get the path length */
@@ -145,18 +144,20 @@ char *path_sram (uint8_t rom_hash [HASH_LENGTH])
 
 /*
  * Generate the path for a save slot.
- *
  * This string needs to be freed.
  */
 char *path_save_slot (uint32_t slot, uint8_t rom_hash [HASH_LENGTH])
 {
+    static char *dir = NULL;
     char *path;
-    char *dir;
     int len;
 
-    if (path_get_dir (&dir, "state") == -1)
+    if (dir == NULL)
     {
-        return NULL;
+        if (path_get_dir (&dir, "state") == -1)
+        {
+            return NULL;
+        }
     }
 
     /* Get the path length */
