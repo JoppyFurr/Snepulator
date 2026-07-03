e0fde98ac650e743b5acf0081d63c627d3e20218
Joppy Furr <joppyfurr@gmail.com>
2026-07-04 11:13:09 +1200

    test: Use ccache when compiling the sst test programs
    Previously, a full recompile was done both for z80 and m68k every time
    the build script was run.
    
    Now, each source file is compiled to a separate object file, some usable
    both by z80 and m68k, and caching is used to skip rebuilding code that
    hasn't changed.

--- tests/build.sh
+++ tests/build.sh
@@ -6,24 +6,44 @@ set -e
 CC=gcc
 CFLAGS="-std=c17 -O2 -Wall -Werror"
 
-echo "Building z80-sst..."
-$CC $CFLAGS \
-    z80-sst.c \
-    util.c \
-    snepulator_compat.c \
-    ../source/cpu/z80.c \
-    ../libraries/cJSON-1.7.19/cJSON.c \
-    -Werror \
-    -o z80-sst
+# Caching
+if command -v ccache > /dev/null
+then
+    echo "Using ccache"
+    CC="ccache $CC"
+    CXX="ccache $CXX"
+fi
 
-echo "Building m68k-sst..."
-$CC $CFLAGS \
-    m68k-sst.c \
-    util.c \
-    snepulator_compat.c \
-    ../source/cpu/m68k.c \
-    ../libraries/cJSON-1.7.19/cJSON.c \
-    -Werror \
-    -o m68k-sst
+# Create a build directory if it does not already exist.
+mkdir -p work
+
+# Compile all objects.
+echo "Compiling... "
+eval $CC $CFLAGS -c ../libraries/cJSON-1.7.19/cJSON.c   -o work/cJSON.o
+eval $CC $CFLAGS -c ../source/cpu/m68k.c                -o work/m68k.o
+eval $CC $CFLAGS -c ../source/cpu/z80.c                 -o work/z80.o
+eval $CC $CFLAGS -c ./snepulator_compat.c               -o work/snepulator_compat.o
+eval $CC $CFLAGS -c ./util.c                            -o work/util.o
+eval $CC $CFLAGS -c ./z80-sst.c                         -o work/z80-sst.o
+eval $CC $CFLAGS -c ./m68k-sst.c                        -o work/m68k-sst.o
+
+# Link the binaries
+echo "Linking..."
+
+$CC $CFLAGS work/z80-sst.o \
+            work/util.o \
+            work/snepulator_compat.o \
+            work/z80.o \
+            work/cJSON.o \
+            -Werror \
+            -o z80-sst
+
+$CC $CFLAGS work/m68k-sst.o \
+            work/util.o \
+            work/snepulator_compat.o \
+            work/m68k.o \
+            work/cJSON.o \
+            -Werror \
+            -o m68k-sst
 
 echo "Done."
